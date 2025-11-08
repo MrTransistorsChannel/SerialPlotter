f13ae8979b0c84dfc9306659f392d23574eb3a17
MrTransistor <mrtransistor0108@gmail.com>
2025-11-08 06:26:52 +0300

    Minor refactoring

--- src/GraphPanel.java
+++ src/GraphPanel.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class GraphPanel extends JPanel {
     private final ArrayList<GraphLine> graphLines = new ArrayList<>();
-    private double minX = 0, maxX = 0, minY = 0, maxY = 0;
 
     public int lineCount() {
         return graphLines.size();
@@ -24,22 +23,6 @@ public class GraphPanel extends JPanel {
         return graphLines.get(id);
     }
 
-    public double getMinX() {
-        return minX;
-    }
-
-    public double getMaxX() {
-        return maxX;
-    }
-
-    public double getMinY() {
-        return minY;
-    }
-
-    public double getMaxY() {
-        return maxY;
-    }
-
     @Override
     public void paintComponent(Graphics g1) {
         Graphics2D g = (Graphics2D) g1;
@@ -53,10 +36,10 @@ public class GraphPanel extends JPanel {
 
         //TODO: allocate space for tick labels, legend and stuff, draw ticks and legend
 
-        minX = Double.POSITIVE_INFINITY;
-        maxX = Double.NEGATIVE_INFINITY;
-        minY = Double.POSITIVE_INFINITY;
-        maxY = Double.NEGATIVE_INFINITY;
+        double minX = Double.POSITIVE_INFINITY;
+        double maxX = Double.NEGATIVE_INFINITY;
+        double minY = Double.POSITIVE_INFINITY;
+        double maxY = Double.NEGATIVE_INFINITY;
         for (GraphLine graphLine : graphLines) {
             if (graphLine.getMinX() < minX) minX = graphLine.getMinX();
             if (graphLine.getMaxX() > maxX) maxX = graphLine.getMaxX();
